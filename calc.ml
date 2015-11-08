11b3f3196e1904b02b292b8c3da7b86d7f827714
Jacob <jacobagraff@gmail.com>
2015-11-08 16:40:03 -0500

    print now compiled to c. make helloWorld to test

--- calc.ml
+++ calc.ml
@@ -6,17 +6,20 @@ let strip_both_chars str =
     | len -> String.sub str 1 (len - 2)
 
 let rec eval = function 
-	Lit(x) -> print_endline x
-|	Int(x) -> print_endline (string_of_int (x/2))
-|	Flt(x) -> print_endline (string_of_float (sqrt x))
-|	Str(x) -> print_endline x
-|	FunCall("print", y) -> print_endline (strip_both_chars y)
-|	FunCall(x, y) -> print_endline ("call: " ^ x ^ " on " ^ y)
-|	Seq(v1, v2) -> eval v1; eval v2
+	Lit(x) -> x
+|	Int(x) -> (string_of_int (x/2))
+|	Flt(x) -> (string_of_float (sqrt x))
+|	Str(x) -> x
+|	FunCall("print", y) -> ("printf(" ^ y ^ ");\n")
+|	FunCall(x, y) -> ("" ^ x ^ "(" ^ y ^ ");\n")
+|	Seq(v1, v2) -> (eval v1) ^ (eval v2)
 
 
 let _ = 
 	let lexbuf = Lexing.from_channel stdin in
 	let expr = Parser.expr Scanner.token lexbuf in
 	let result = eval expr in
-	result
+	print_endline "#include <stdio.h>\n";
+	print_endline "int main(int argc, char **argv) {\n";
+	print_endline result;
+	print_endline "}"
