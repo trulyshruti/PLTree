7128e823b91fb8c7e3945dbb6346d3c67b1c3cc3
Luis Ramirez <lurmz94@gmail.com>
2015-11-14 22:11:55 -0500

    update tester script; accepts dirs & multi args

--- tester.sh
+++ tester.sh
@@ -18,59 +18,66 @@ exp='_exp'
 outdict='output_out/'
 expdict='output_exp/'
 
-# Make sure there's only one argument
-if ! [ $# -eq 1 ]
+# Make sure there's arguments
+if [ $# -eq 0 ]
 then
-	echo 'usage: ./tester.sh FILE.tree'
-	exit 2;
-# Make sure file is in directory
-elif ! [ -a ${1} ]
-then
-	echo 'Place FILE.tree in this directory.'
+	echo 'usage: ./tester.sh [FILE.tree | treedir/]?'
 	exit 2;
 # Make sure calc is in directory
 elif ! [ -a calc ]
 then
 	echo 'Invoke lexer/parser to produce calc file.'
 	exit 2;
-# Make sure an expected output file is present
-elif ! [ -a ${expdict}${1%.*}${exp} ]
-then
-	echo 'Place FILE_exp file in output_exp/ directory.'
-	exit 2;
 else
-	# remove .tree extension
-	progname="${1%.*}"
-	# run parser on input file; save output
-	cat $1 | ./calc > "${outdict}${progname}${out}"
+	for arg
+	do
+		# if it's a regular file
+		if [ -f $arg ]
+		then
+			# remove .tree extension
+			progname="${arg%.*}"
+			# Make sure an expected output file is present
+			if ! [ -a ${expdict}${progname}${exp} ]
+			then
+				echo "Place ${progname}_exp file in output_exp/ directory."
+				exit 2;
+			else
+				# run parser on input file; save output
+				cat $arg | ./calc > "${outdict}${progname}${out}"
 
-	# check if there are diffs
-	if ! [[ $(diff ${outdict}${progname}${out} ${expdict}${progname}${exp}) ]]
-	then
-		echo 'NO DIFF FOUND'
-		exit 0;
-	else
-		echo 'DIFFS DETECTED'
-		echo '-------------------------'
-		echo 'view with: '
-		echo '(1) diff (default)'
-		echo '(2) diff (context)'
-		echo '(3) vim -d'
-		echo 'enter [1,2,3] and press [ENTER]: '
-		read choice
-		echo -e "\n"
-		if [ $choice -eq 1 ]
+				# check if there are diffs
+				if ! [[ $(diff ${outdict}${progname}${out} ${expdict}${progname}${exp}) ]]
+				then
+					printf "${progname}: \033[0;32mSUCCESS\033[0m\n"
+				else
+					printf "${progname}: \033[0;31mFAILED\033[0m\n"
+				fi
+			fi
+		# if it's a directory
+		elif [ -d $arg ]
 		then
-			diff ${outdict}${progname}${out} ${expdict}${progname}${exp}
-		elif [ $choice -eq 2 ]
-		then
-			diff -c ${outdict}${progname}${out} ${expdict}${progname}${exp}
-		elif [ $choice -eq 3 ]
-		then
-			vim -d ${outdict}${progname}${out} ${expdict}${progname}${exp}
-		else
-			echo 'invalid choice. exiting.'
-			exit 2;
+			for f in $(ls $arg)
+			do
+				# remove .tree extension
+				progname="${f%.*}"
+				# Make sure an expected output file is present
+				if ! [ -a ${expdict}${progname}${exp} ]
+				then
+					echo "Place ${progname}_exp file in output_exp/ directory."
+					exit 2;
+				else
+					# run parser on input file; save output
+					cat $arg/$f | ./calc > "${outdict}${progname}${out}"
+
+					# check if there are diffs
+					if ! [[ $(diff ${outdict}${progname}${out} ${expdict}${progname}${exp}) ]]
+					then
+						printf "${arg}${progname}: \033[0;32mSUCCESS\033[0m\n"
+					else
+						printf "${arg}${progname}: \033[0;31mFAILED\033[0m\n"
+					fi
+				fi
+			done
 		fi
-	fi
+	done
 fi
