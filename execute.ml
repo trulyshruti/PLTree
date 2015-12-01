9c1283bba38c5613249ed57852ac2431277f536f
Justin Walters <jw3043@columbia.edu>
2015-12-01 03:09:13 -0500

    Preparing for top-level func declarations
    Currently just prints out whole SAST

--- execute.ml
+++ execute.ml
@@ -8,11 +8,16 @@ let get_vars prog =
   get_vars_list prog
 let print_vars vars = string_of_program vars
 
-let get_funcs prog = prog
-let print_funcs funcs = "funcs"
+let get_funcs prog =
+   (* let rec get_funcs_list = function
+    [] -> []
+    | hd::tl -> match hd with FuncDec(_,_) -> hd::get_funcs_list tl
+              | _ -> get_funcs_list tl in
+  get_vars_list *) prog
+let print_funcs funcs = string_of_program funcs
 
 let execute_prog prog =
   let vars = get_vars prog in
   let funcs = get_funcs prog in
   ignore(print_endline ("Variables: " ^ print_vars vars));
-  ignore(print_endline (print_funcs funcs));
+  ignore(print_endline ("Funcs: " ^ print_funcs funcs));
