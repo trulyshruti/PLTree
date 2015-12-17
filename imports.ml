c8f1f6397f9dfdeb5e617204720ae445f544e127
Jacob <jacobagraff@gmail.com>
2015-12-17 14:01:06 -0500

    Fix some warnings

--- imports.ml
+++ imports.ml
@@ -10,16 +10,15 @@ let rec lex_file filename outfile opened_list =
 		function
 		Iast.Open(s)::tl -> 	if List.exists (function a -> s = a) l then (
 						print_program outfile l tl
-					) else (
-						lex_file s outfile (s::l); 
-						print_program outfile (s::l) tl
+					) else (	
+						print_program outfile (lex_file s outfile (s::l)) tl
 					)
 	|	Iast.Other(c)::tl -> fprintf outfile "%c" c; print_program outfile l tl
 	|	Iast.String(s)::tl -> fprintf outfile "%s" ("\"" ^ s ^ "\""); print_program outfile l tl
-	|	[] -> () in
+	|	[] -> l in
 	print_program outfile opened_list program
 	
 
 let check_imports infilename outfilename = 
 	let out = open_out outfilename in
-	lex_file infilename out []; close_out out
+	ignore (lex_file infilename out []); close_out out
