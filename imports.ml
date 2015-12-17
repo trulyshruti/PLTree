e99df2e69376a6605124c3ace60dcbe72d1d6c05
Jacob <jacobagraff@gmail.com>
2015-12-17 00:04:20 -0500

    Add double import checking

--- imports.ml
+++ imports.ml
@@ -1,18 +1,25 @@
 open Printf
 
-let rec lex_file filename outfile =
+
+let rec lex_file filename outfile opened_list =
   	let lexbuf = Lexing.from_channel (open_in filename) in
 	let program = Iparser.program Iscanner.token lexbuf in
 	let rec print_program = 
 		function outfile ->
+		function l -> 
 		function
-		Iast.Open(s)::tl -> lex_file s outfile; print_program outfile tl
-	|	Iast.Other(c)::tl -> fprintf outfile "%c" c; print_program outfile tl
-	|	Iast.String(s)::tl -> fprintf outfile "%s" ("\"" ^ s ^ "\""); print_program outfile tl
+		Iast.Open(s)::tl -> 	if List.exists (function a -> s = a) l then (
+						print_program outfile l tl
+					) else (
+						lex_file s outfile (s::l); 
+						print_program outfile (s::l) tl
+					)
+	|	Iast.Other(c)::tl -> fprintf outfile "%c" c; print_program outfile l tl
+	|	Iast.String(s)::tl -> fprintf outfile "%s" ("\"" ^ s ^ "\""); print_program outfile l tl
 	|	[] -> () in
-	print_program outfile program
+	print_program outfile opened_list program
 	
 
 let check_imports infilename outfilename = 
 	let out = open_out outfilename in
-	lex_file infilename out; close_out out
+	lex_file infilename out []; close_out out
